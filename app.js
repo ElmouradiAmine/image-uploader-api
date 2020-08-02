f42a2abd012ac6d609ec187a3bf08d2cbf55caa5
elmouradiAmine <elmouradi.amine98@gmail.com>
2020-08-02 14:15:27 +0100

    Adding mongodb connection

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const imageRoute = require('./api/routes/images');
+
+
 const app = express();
+dotenv.config();
+
+
+const mongoUri = process.env.ENV === 'DEV' ? process.env.MONGO_DEV_URI : process.env.MONGO_PROD_URI;
+
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to database successfully.');
+    }
+})
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
