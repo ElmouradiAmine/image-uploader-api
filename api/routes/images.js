3bf26f1f47a70f6f6f4d057f1d1d6c36666fad42
elmouradiAmine <elmouradi.amine98@gmail.com>
2020-08-02 14:03:15 +0100

    updated put to patch

--- api/routes/images.js
+++ api/routes/images.js
@@ -6,7 +6,7 @@ const imagesController = require('../controllers/images')
 //This router is prefixed by /images
 router.get('/', imagesController.get_all_images);
 router.post('/', imagesController.create_image);
-router.put('/:imageId', imagesController.update_image);
+router.patch('/:imageId', imagesController.update_image);
 router.delete('/:imageId', imagesController.delete_image);
 
 module.exports = router;
